acb5e4806284b0a1831a9c5f3ac469e5a1b9a4d2
MerveMay <mmervecelik36@gmail.com>
2022-07-29 10:50:14 -0400

    class26

--- src/Class26/Task5.java
+++ src/Class26/Task5.java
@@ -1,24 +1,27 @@
 package Class26;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Task5 {
     public static void main(String[] args) {
         //Create an arrayList of even numbers from 1 to 500. Remove any number that is divisible by 5 from that arrayList.
-        ArrayList<Integer> evenNumbers= new ArrayList<>();
-        for(int i=0; i<=500; i++){
-            if (i%2==0){
-                evenNumbers.add(i);
+        ArrayList<Integer> evenNumbers=new ArrayList<>();
+
+        for (int i = 2; i <500 ; i+=2) {
+            evenNumbers.add(i);
+        }
+        System.out.println(evenNumbers);
+        Iterator<Integer> iterator=evenNumbers.iterator();
+        while (iterator.hasNext()){
+            if(iterator.next()%5==0){
+                iterator.remove();
             }
         }
-        System.out.print("Even numbers on ArrayList: "+evenNumbers+" ");
-        System.out.println("--------------------------------------------------------------------------------------------");
-        for (int i=0; i<evenNumbers.size(); i++){
-            if (evenNumbers.get(i)%5==0){
-                evenNumbers.remove(i);
-            }
-            System.out.print("Even numbers which cannot divisible by 5: "+ evenNumbers+" ");
-        }
+
+        //  evenNumbers.removeIf(num->num%5==0);
+        System.out.println(evenNumbers);
+
 
 
     }
